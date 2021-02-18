14f50846107f7bbe04ca1ab12cbbe7e898aed6d8
Myroslav Tiuk <fagost606@gmail.com>
2021-02-18 12:23:51 +0200

    done detail Screen

--- screens/MealDetailScreen.js
+++ screens/MealDetailScreen.js
@@ -1,47 +1,72 @@
-import React, {useLayoutEffect} from 'react';
-import {Text, View, StyleSheet, Button, TouchableOpacity} from 'react-native';
+import React, { useLayoutEffect } from 'react';
+import { Text, View, StyleSheet, ScrollView, TouchableOpacity, Image } from 'react-native';
 import MealItem from '../components/MealItem';
-import {MEALS} from '../data/dummy-data';
-import {HeaderButtons, Item} from 'react-navigation-header-buttons';
+import { MEALS } from '../data/dummy-data';
+import { HeaderButtons, Item } from 'react-navigation-header-buttons';
 import HeaderButton from '../components/HeaderButtons';
 import Ionicons from 'react-native-vector-icons/Ionicons';
 
-const MealDetailScreen = ({navigation, route}) => {
+const MealDetailScreen = ({ navigation, route }) => {
+
+  const mealId = route.params.mealId;
+  const selectedMeal = MEALS.find((meal) => meal.id === mealId);
+
   useLayoutEffect(() => {
     const iconName = 'star-outline';
     navigation.setOptions({
       headerRight: () => (
-        <TouchableOpacity>
+        <TouchableOpacity onPress={() => console.warn('we are here')}>
           <Ionicons
-            style={{paddingRight: 10}}
+            style={{ paddingRight: 10 }}
             name={iconName}
             size={30}
-            onPress={() => console.warn('here we are')}
           />
         </TouchableOpacity>
       ),
     });
   }, [navigation]);
 
-  const mealId = route.params.mealId;
-  console.log(mealId);
-  console.log(MEALS);
 
-  const selectedMeal = MEALS.find((meal) => meal.id === mealId);
-  console.log(selectedMeal, 'selectedMeal');
   return (
-    <View style={styles.container}>
-      <Text>{selectedMeal.tittle}</Text>
-    </View>
+    <ScrollView>
+      <Image source={{ uri: selectedMeal.imageUrl }} style={styles.image} />
+      <View style={styles.detail}>
+        <Text>{selectedMeal.duration}min</Text>
+        <Text>{selectedMeal.complexity.toUpperCase()}</Text>
+        <Text>{selectedMeal.affordability.toUpperCase()}</Text>
+      </View>
+      <Text style={styles.title}>Ingredients</Text>
+      {selectedMeal.ingredients.map(ingredient => <Text style={styles.list} key={ingredient}>{ingredient}</Text>)}
+      <Text style={styles.title}>Steps</Text>
+      {selectedMeal.steps.map(step => <Text style={styles.list} key={step}>{step}</Text>)}
+    </ScrollView>
   );
 };
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+  image: {
+    width: '100%',
+    height: 200
   },
+  detail: {
+    flexDirection: 'row',
+    padding: 15,
+    justifyContent: 'space-around'
+  },
+  title: {
+    fontSize: 22,
+    textAlign: 'center',
+    fontWeight: '600'
+  },
+  list: {
+    marginHorizontal: 10,
+    marginVertical: 7,
+    borderWidth: 1,
+    borderRadius: 10,
+    paddingLeft: 20,
+    paddingVertical: 3,
+    borderColor: '#ccc'
+  }
 });
 
 export default MealDetailScreen;
